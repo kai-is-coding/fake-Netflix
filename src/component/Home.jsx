18e33effa6d50a3d558caaad2c7723360dc20221
Kai Yang <kaiyang.ky2@gmail.com>
2020-09-08 17:38:31 +1000

    horizontal scroolling working for both movie and tv trending box

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,42 +1,18 @@
-import React, { useState, useEffect, Fragment } from "react";
-import http from "../services/httpService";
+import React, { Fragment } from "react";
+import TrendingBox from "./TrendingBox";
+// import http from "../services/httpService";
 
 function Home(props) {
-  const [data, setData] = useState([]);
-
-  async function getData() {
-    const { data } = await http.get(
-      `trending/movies/week?api_key=${http.api_key}`
-    );
-    console.log(data.results[0]);
-    setData(data.results);
-  }
-
-  function getImagesURL(path) {
-    return `https://image.tmdb.org/t/p/w200/${path}`;
-  }
-
-  useEffect(() => {
-    getData();
-    // console.log("data", data);
-  }, []);
-
   return (
     <Fragment>
-      <div className="container">
+      <div className="container" style={{ display: "block" }}>
         <div className="row">
-          {/* <div className="col">Movie</div> */}
-          {data.map((item) => {
-            return (
-              <div className="col" key={item.id} style={{ margin: 10 }}>
-                <img
-                  src={getImagesURL(item.poster_path)}
-                  alt={item.title}
-                  className="float-left"
-                />
-              </div>
-            );
-          })}
+          <h2>Movie</h2>
+          <TrendingBox mediaType="movie" />
+        </div>
+        <div className="row">
+          <h2>TV</h2>
+          <TrendingBox mediaType="tv" />
         </div>
       </div>
     </Fragment>
---
+++ src/component/TrendingBox.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import http from "../services/httpService";
+
+import "../css/TrendingBox.css";
+
+function TrendingBox({ mediaType }) {
+  // console.log("props", props);
+  const [data, setData] = useState([]);
+
+  async function getData() {
+    const { data } = await http.get(
+      `trending/${mediaType}/week?api_key=${http.api_key}`
+    );
+    // console.log(data.results[0]);
+    setData(data.results);
+  }
+
+  function getImagesURL(path) {
+    return `https://image.tmdb.org/t/p/w200/${path}`;
+  }
+
+  useEffect(() => {
+    getData();
+  }, [data]);
+
+  return (
+    <div>
+      <div className="container">
+        {data.map((item) => {
+          return (
+            <img
+              key={item.id}
+              src={getImagesURL(item.poster_path)}
+              alt={item.title}
+              className="images"
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default TrendingBox;
