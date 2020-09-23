ca31ec08cd38be42ea21bb984f332d5d7b9293ff
Kai Yang <kaiyang.ky2@gmail.com>
2020-09-24 09:34:46 +1000

    fixed missing dependency error in Details.jsx

--- src/component/Details.jsx
+++ src/component/Details.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 
 import http from "../services/httpService";
 
@@ -6,7 +6,7 @@ const Details = ({ match, history }) => {
   const { id } = match.params;
   const { pathname } = history.location;
 
-  async function getData() {
+  const getData = useCallback(async () => {
     if (pathname.startsWith("/movie")) {
       const { data } = await http.get(
         `movie/${id}?api_key=${http.api_key}&language=en-US`
@@ -18,11 +18,11 @@ const Details = ({ match, history }) => {
       );
       console.log(data);
     }
-  }
+  }, [pathname, id]);
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [getData]);
   return (
     <div>
       <h1>Detail of {id}</h1>
